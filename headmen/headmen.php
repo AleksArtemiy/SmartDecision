e2e341d832d602ca1cab9f0d8105dd09feaf432e
Temiiin <alekseev3artem@gmail.com>
2025-11-20 03:19:51 +0300

    Repair headmen styles

--- headmen/headmen.php
+++ headmen/headmen.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Журнал 2.0 - Расписание и Статистика</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/headmen_styles.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/headmen_styles.css">
 </head>
 <body>
 <!-- Шапка -->
