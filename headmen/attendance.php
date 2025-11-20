0063046190a189d01a804f531ddd0307d4be4b58
Temiiin <alekseev3artem@gmail.com>
2025-11-20 03:30:56 +0300

    refactor attendance v2

--- headmen/attendance.php
+++ headmen/attendance.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Журнал посещаемости 2.0</title>
-    <link rel="stylesheet" href="../styles/universal.css">
-    <link rel="stylesheet" href="../styles/attendance_styles.css">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/attendance.css">
 </head>
 <body>
 <!-- Шапка -->
